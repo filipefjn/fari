39259ba25e5933c41ca7d55f707a18287c017dbf
Filipe Jordan do Nascimento <filipe.fjn@gmail.com>
2020-01-20 20:38:45 -0300

    added font-awesome icons

--- frontend/src/components/PlaybackControls.vue
+++ frontend/src/components/PlaybackControls.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="playback-controls-container">
-        <button class="button"></button>
-        <button class="button play"></button>
-        <button class="button"></button>
+        <button class="button"><fa-icon icon="backward" style="margin-right: 1px"/></button>
+        <button class="button play"><fa-icon icon="play" style="margin-left: 2px"/></button>
+        <button class="button"><fa-icon icon="forward" style="margin-left: 1px"/></button>
     </div>
 </template>
 
@@ -27,9 +27,18 @@ export default {
         border: none;
         outline: none;
         background-color: #2A2A2A;
+        color: $text-color;
         box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.15), -2px -2px 2px rgba(255, 255, 255, 0.04);
         margin: 0 0.25rem 0 0.25rem;
         cursor: pointer;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 0.75rem;
+
+        &:active {
+            box-shadow: inset 2px 2px 2px rgba(0, 0, 0, 0.15), inset -2px -2px 2px rgba(255, 255, 255, 0.04);
+        }
 
         &.play {
             height: 2rem;
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+// font awesome icons
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPlay, faForward, faBackward } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+library.add(faPlay, faForward, faBackward)
+
+Vue.component('fa-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false
 
 new Vue({
