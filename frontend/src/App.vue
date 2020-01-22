9cfb1651c8b64d46d986a57645c53807a7d617e8
Filipe Jordan do Nascimento <filipe.fjn@gmail.com>
2020-01-22 12:03:01 -0300

    added scrollbar css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -19,4 +19,43 @@ export default {
 </script>
 
 <style lang="scss">
+@import '@/style.scss';
+
+// font import
+@import url('https://fonts.googleapis.com/css?family=Monda&display=swap');
+
+// style applied to all html tags
+* {
+    font-family: 'Monda', sans-serif;
+    line-height: 1.5;
+    box-sizing: border-box;
+}
+
+// remove firefox's button inner border
+button::-moz-focus-inner {
+    border: 0;
+}
+
+// scrollbar css for chrome/chromium
+::-webkit-scrollbar {
+  width: 12px;
+}
+
+::-webkit-scrollbar-track {
+  background: #2A2A2A;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #444444;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #444444;
+}
+
+// scrollbar css for other browsers
+* {
+    scrollbar-color: #444444 #2A2A2A;
+}
+
 </style>
