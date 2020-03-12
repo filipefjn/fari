bcd37b60efce04cdcdac98085dbe5bf3c0e65036
Filipe Jordan do Nascimento <filipe.fjn@gmail.com>
2020-03-12 09:47:38 -0300

    reimplemented 'VolumeSlider' with input range

--- frontend/src/components/VolumeSlider.vue
+++ frontend/src/components/VolumeSlider.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="container" :style="containerStyle">
-        <fa-icon icon="volume-up" class="volume-icon"/>
-        <div class="volume" @click="onClick($event)" @wheel="onWheel($event)">
-            <div class="slider" :style="sliderStyle"></div>
+        <fa-icon icon="volume-up" class="volume-icon" />
+        <div class="volume">
+            <input type="range" class="slider" v-model="value" @wheel="onWheel($event)"/>
         </div>
     </div>
 </template>
@@ -17,59 +17,48 @@ export default {
             default: 160
         }
     },
+    data: function() {
+        return {
+            value: 100,
+        };
+    },
     computed: {
         ...mapGetters(["playerVolume"]),
         containerStyle: function() {
             return {
                 'width': this.width,
             }
-        },
-        sliderStyle: function() {
-            let volume = this.playerVolume;
-            if(volume === undefined || volume === null) {
-                volume = 1;
-            }
-            return {
-                'width': (volume * 100) + "%"
-            }
         }
     },
     methods: {
-        onClick: function(event) {
-            let volume = event.layerX - 5;
-            let max = this.width - 10;
-            if(volume > max) {
-                volume = max;
-            } else if(volume < 0) {
-                volume = 0;
-            }
-            volume = volume / max;
-            this.$store.dispatch('setPlayerVolume', volume);
-        },
         onWheel: function(event) {
-            let volume = this.playerVolume;
             if(event.deltaY < 0) {
-                volume = volume + 0.1;
-                if(volume > 1) {
-                    volume = 1;
-                }
+                this.value += 10;
             } else if(event.deltaY > 0) {
-                volume = volume - 0.1;
-                if(volume < 0) {
-                    volume = 0;
-                }
+                this.value -= 10;
             }
-            this.$store.dispatch('setPlayerVolume', volume);
+        }
+    },
+    watch: {
+        value: function(val) {
+            this.$store.dispatch("setPlayerVolume", (val / 100));
+        },
+        playerVolume: {
+            handler: function(val) {
+                if((this.value / 100) !== val) {
+                    this.value = (val * 100);
+                }
+            },
+            immediate: true
         }
     }
-}
+};
 </script>
 
 <style scoped lang="scss">
-@import '@/style.scss';
+@import "@/style.scss";
 
 .container {
-
     position: relative;
     padding: 0;
     width: 160px;
@@ -94,7 +83,6 @@ export default {
         box-shadow: $button-shadow-pressed;
         display: flex;
         align-items: center;
-        cursor: pointer;
 
         .slider {
             background-color: white;
@@ -104,5 +92,93 @@ export default {
     }
 }
 
-
+input[type="range"] {
+    height: 18px;
+    -webkit-appearance: none;
+    margin: 10px 0;
+    width: 100%;
+}
+input[type="range"]:focus {
+    outline: none;
+}
+input[type="range"]::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 4px;
+    cursor: pointer;
+    animate: 0.2s;
+    box-shadow: 0px 0px 0px #000000;
+    background: rgb(156, 156, 156);
+    border-radius: 5px;
+    border: 0px solid #000000;
+}
+input[type="range"]::-webkit-slider-thumb {
+    box-shadow: 0px 0px 0px #000000;
+    border: 0px solid #000000;
+    height: 12px;
+    width: 12px;
+    border-radius: 6px;
+    background: #ffffff;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -3px;
+}
+input[type="range"]:focus::-webkit-slider-runnable-track {
+    background: rgb(156, 156, 156);
+}
+input[type="range"]::-moz-range-track {
+    width: 100%;
+    height: 4px;
+    cursor: pointer;
+    animate: 0.2s;
+    box-shadow: 0px 0px 0px #000000;
+    background: rgb(156, 156, 156);
+    border-radius: 5px;
+    border: 0px solid #000000;
+}
+input[type="range"]::-moz-range-thumb {
+    box-shadow: 0px 0px 0px #000000;
+    border: 0px solid #000000;
+    height: 12px;
+    width: 12px;
+    border-radius: 6px;
+    background: #ffffff;
+    cursor: pointer;
+}
+input[type="range"]::-ms-track {
+    width: 100%;
+    height: 6px;
+    cursor: pointer;
+    animate: 0.2s;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+}
+input[type="range"]::-ms-fill-lower {
+    background: rgb(156, 156, 156);
+    border: 0px solid #000000;
+    border-radius: 10px;
+    box-shadow: 0px 0px 0px #000000;
+}
+input[type="range"]::-ms-fill-upper {
+    background: rgb(156, 156, 156);
+    border: 0px solid #000000;
+    border-radius: 10px;
+    box-shadow: 0px 0px 0px #000000;
+}
+input[type="range"]::-ms-thumb {
+    margin-top: 1px;
+    box-shadow: 0px 0px 0px #000000;
+    border: 0px solid #000000;
+    height: 12px;
+    width: 12px;
+    border-radius: 5px;
+    background: #ffffff;
+    cursor: pointer;
+}
+input[type="range"]:focus::-ms-fill-lower {
+    background: rgb(156, 156, 156);
+}
+input[type="range"]:focus::-ms-fill-upper {
+    background: rgb(156, 156, 156);
+}
 </style>
